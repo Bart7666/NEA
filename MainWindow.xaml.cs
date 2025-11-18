dee1f2e8d83aa327a8653ada1513fc993aacb5be
Bartosz Wegrzycki <bartosz.wegrzycki@outlook.com>
2025-11-18 22:15:24 +0000

    Ui ing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,35 @@ namespace NEA
         {
             InitializeComponent();
         }
+
+        private void MinimiseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximiseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState == WindowState.Maximized) { this.WindowState= WindowState.Normal; }
+            else {  this.WindowState = WindowState.Maximized;}
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow settingsWindow = new SettingsWindow();
+            settingsWindow.Owner = this;
+            settingsWindow.Show();
+            this.Hide();
+            
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
---
+++ SettingsWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NEA
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)LightModeRButton.IsChecked)
+            {
+                Application.Current.Resources["Button"] = Application.Current.Resources["BaseButton"];
+                Application.Current.Resources["Menu"] = Application.Current.Resources["BaseMenu"];
+                Application.Current.Resources["HMenu"] = Application.Current.Resources["BaseHMenu"];
+                Application.Current.Resources["MenuItem"] = Application.Current.Resources["BaseMenuItem"];
+                Application.Current.Resources["Border"] = Application.Current.Resources["BaseBorder"];
+                Application.Current.Resources["GridBorders"] = Application.Current.Resources["BaseGridBorders"];
+                Application.Current.Resources["Window"] = Application.Current.Resources["BaseWindow"];
+                Application.Current.Resources["TextBlock"] = Application.Current.Resources["BaseTextBlock"];
+                Application.Current.Resources["Label"] = Application.Current.Resources["BaseLabel"];
+                Application.Current.Resources["TextBox"] = Application.Current.Resources["BaseTextBox"];
+                Application.Current.Resources["RadioButton"] = Application.Current.Resources["BaseRadioButton"];
+            }
+            else if ((bool)DarkModeRButton.IsChecked)
+            {
+                Application.Current.Resources["Button"] = Application.Current.Resources["DarkButton"];
+                Application.Current.Resources["Menu"] = Application.Current.Resources["DarkMenu"];
+                Application.Current.Resources["HMenu"] = Application.Current.Resources["DarkHMenu"];
+                Application.Current.Resources["MenuItem"] = Application.Current.Resources["DarkMenuItem"];
+                Application.Current.Resources["Border"] = Application.Current.Resources["DarkBorder"];
+                Application.Current.Resources["GridBorders"] = Application.Current.Resources["DarkGridBorders"];
+                Application.Current.Resources["Window"] = Application.Current.Resources["DarkWindow"];
+                Application.Current.Resources["TextBlock"] = Application.Current.Resources["DarkTextBlock"];
+                Application.Current.Resources["Label"] = Application.Current.Resources["DarkLabel"];
+                Application.Current.Resources["TextBox"] = Application.Current.Resources["DarkTextBox"];
+                Application.Current.Resources["RadioButton"] = Application.Current.Resources["DarkRadioButton"];
+            }
+                this.Owner.Show();
+            this.Close();
+        }
+    }
+}
