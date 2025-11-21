5028d1265e390f4224a6b7c70b64079afecafa85
Bartosz Wegrzycki <bartosz.wegrzycki@outlook.com>
2025-11-21 09:57:45 +0000

    Settingsing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,7 @@ namespace NEA
         public MainWindow()
         {
             InitializeComponent();
+            this.WindowState = WindowState.Maximized;
         }
 
         private void MinimiseButton_Click(object sender, RoutedEventArgs e)
--- SettingsWindow.xaml.cs
+++ SettingsWindow.xaml.cs
@@ -22,40 +22,66 @@ namespace NEA
         public SettingsWindow()
         {
             InitializeComponent();
+            this.WindowState = WindowState.Maximized; //Window will be fullscreen upon opening
         }
+        
 
+
+        /// <summary>
+        /// Sets the user theme depending on the currently selected radiobutton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ApplyButton_Click(object sender, RoutedEventArgs e)
         {
-            if ((bool)LightModeRButton.IsChecked)
+            if (LightModeRButton.IsChecked != null && (bool)LightModeRButton.IsChecked)
             {
+                this.Owner.Resources["Theme"] = "Light";//This changes the value of theme to light
                 Application.Current.Resources["Button"] = Application.Current.Resources["BaseButton"];
                 Application.Current.Resources["Menu"] = Application.Current.Resources["BaseMenu"];
-                Application.Current.Resources["HMenu"] = Application.Current.Resources["BaseHMenu"];
                 Application.Current.Resources["MenuItem"] = Application.Current.Resources["BaseMenuItem"];
                 Application.Current.Resources["Border"] = Application.Current.Resources["BaseBorder"];
-                Application.Current.Resources["GridBorders"] = Application.Current.Resources["BaseGridBorders"];
                 Application.Current.Resources["Window"] = Application.Current.Resources["BaseWindow"];
                 Application.Current.Resources["TextBlock"] = Application.Current.Resources["BaseTextBlock"];
                 Application.Current.Resources["Label"] = Application.Current.Resources["BaseLabel"];
                 Application.Current.Resources["TextBox"] = Application.Current.Resources["BaseTextBox"];
                 Application.Current.Resources["RadioButton"] = Application.Current.Resources["BaseRadioButton"];
             }
-            else if ((bool)DarkModeRButton.IsChecked)
+            else if (DarkModeRButton.IsChecked != null && (bool)DarkModeRButton.IsChecked)
             {
+                this.Owner.Resources["Theme"] = "Dark";//This changes the value of theme to dark
                 Application.Current.Resources["Button"] = Application.Current.Resources["DarkButton"];
                 Application.Current.Resources["Menu"] = Application.Current.Resources["DarkMenu"];
-                Application.Current.Resources["HMenu"] = Application.Current.Resources["DarkHMenu"];
                 Application.Current.Resources["MenuItem"] = Application.Current.Resources["DarkMenuItem"];
                 Application.Current.Resources["Border"] = Application.Current.Resources["DarkBorder"];
-                Application.Current.Resources["GridBorders"] = Application.Current.Resources["DarkGridBorders"];
                 Application.Current.Resources["Window"] = Application.Current.Resources["DarkWindow"];
                 Application.Current.Resources["TextBlock"] = Application.Current.Resources["DarkTextBlock"];
                 Application.Current.Resources["Label"] = Application.Current.Resources["DarkLabel"];
                 Application.Current.Resources["TextBox"] = Application.Current.Resources["DarkTextBox"];
                 Application.Current.Resources["RadioButton"] = Application.Current.Resources["DarkRadioButton"];
             }
-                this.Owner.Show();
+            this.Owner.Show();
             this.Close();
+        }
+        /// <summary>
+        /// When the apply button is loaded, this sets the currently enabled themes and settings to be checked within the settings.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ApplyButton_Loaded(object sender, RoutedEventArgs e)
+        {
+            if ((string)this.Owner.FindResource("Theme") == "Light")
+            {
+                LightModeRButton.IsChecked = true;
+            }
+            else if ((string)this.Owner.FindResource("Theme") == "Dark")
+            {
+                DarkModeRButton.IsChecked = true;
+            }
+            else //Base case for first loading
+            {
+                LightModeRButton.IsChecked = true;
+            }
         }
     }
 }
